31fde5d764b159dbfff883235853db4cdd1a7090
Virginie Bosquet <vivi_sander@hotmail.com>
2019-09-06 13:55:55 +0200

    Updating cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
      origins '*'
      resource '*',
-              expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+              expose: %w(access-token expiry token-type uid client),
               headers: :any,
               methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
